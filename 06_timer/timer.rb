48cba6787f74121edfd85b10d9f3253a08881268
Licykk <licykk@gmail.com>
2018-01-07 17:37:13 -0500

    done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+  @@seconds = 0
+  @@minutes = 0
+  @@hours = 0
+  def seconds=(s)
+    if s >= 60
+      @@minutes = s/60
+      @@seconds = s%60
+      if @@minutes >= 60
+        @@hours = @@minutes/60
+        @@minutes = @@minutes%60
+      end
+    else
+      @@seconds = s
+    end
+  end
+
+  def seconds
+    @@seconds
+  end
+
+  def time_string
+    return ("%02d" % @@hours).to_s + ":" + ("%02d" % @@minutes).to_s + ":" + ("%02d" % @@seconds).to_s
+  end
 end
